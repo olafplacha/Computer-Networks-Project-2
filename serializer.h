806634577df47f6fd05a9936e1479aec3aa1fdce
Olaf Placha <olafplacha@gmail.com>
2022-05-21 17:42:40 +0200

    added docstring

--- serializer.h
+++ serializer.h
@@ -1,6 +1,16 @@
 #ifndef SERIALIZER_H
 #define SERIALIZER_H
 
+#include "messages.h"
 
+// Client-specific.
+
+/**
+ * Returns server's message sent over TCP connection.
+ *
+ * @param socket Socket file descriptor.
+ * @return server's message.
+ */
+ServerMessage readServerMessage(int socket);
 
 #endif // SERIALIZER_H

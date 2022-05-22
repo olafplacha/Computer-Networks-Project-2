44d52d30e53214c0ac652ae633f852aa07c936d5
Olaf Placha <olafplacha@gmail.com>
2022-05-22 11:56:07 +0200

    added tcp reading

--- network_handler.cpp
+++ network_handler.cpp
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <cerrno>
+#include <cstring>
 #include "network_handler.h"
 
-TCPHandler::TCPHandler(int socket_, int recv_buff_size_) : socket(socket_), 
+TCPHandler::TCPHandler(int socket_, size_t recv_buff_size_) : socket(socket_), 
     recv_buff_size(recv_buff_size_), recv_deque()
 {
     recv_buff = (char *) malloc(recv_buff_size_);
@@ -13,4 +17,42 @@ TCPHandler::TCPHandler(int socket_, int recv_buff_size_) : socket(socket_),
 TCPHandler::~TCPHandler()
 {
     free(recv_buff);
+}
+
+bool TCPHandler::return_when_n_bytes_in_deque(size_t n)
+{
+    // If there are enough bytes in the recv_deque, then do not read on the socket.
+    while (recv_deque.size() < n) {
+        ssize_t received_bytes = recv(socket, recv_buff, recv_buff_size, 0);
+        // Check if read successful.
+        if (received_bytes < 0) {
+            std::cerr << std::strerror(errno) << '\n';
+            exit(EXIT_FAILURE);
+        }
+        // Check if peer disconnected.
+        if (received_bytes == 0) {
+            return false;
+        }
+        // Copy all read bytes to recv_deque.
+        for (ssize_t i = 0; i < received_bytes; i++)
+        {
+            recv_deque.push_back(recv_buff[i]);
+        }
+    }
+    return true;
+}
+
+bool TCPHandler::read_n_bytes(size_t n, char* buff)
+{
+    bool connected = return_when_n_bytes_in_deque(n);
+    if (!connected) {
+        return false;
+    }
+    // At this point there are at least n bytes in the deque.
+    for (size_t i = 0; i < n; i++)
+    {
+        buff[i] = recv_deque.front();
+        recv_deque.pop_front();
+    }
+    return true;
 }
--- network_handler.h
+++ network_handler.h
@@ -15,19 +15,20 @@ class TCPHandler {
          * @param socket_ Connected TCP socket file descriptor.
          * @param recv_buff_size_ Size of the receive buffer.
          */
-        TCPHandler(int socket_, int recv_buff_size_);
+        TCPHandler(int socket_, size_t recv_buff_size_);
 
         ~TCPHandler();
 
         /**
-         * @brief Read subsequent N bytes from the TCP stream. Note that
-         * this method performs a read on the socket only if it has to.
+         * @brief Read subsequent N bytes from the TCP stream. Note that this method performs 
+         * a read on the socket only if it has to.
          * 
          * @param n Number of bytes to be read.
-         * @param buff Pointer to buffer of size at least N.
-         * @return False iff the peer was disconnected.
+         * @param buff Pointer to buffer of size at least N in which read bytes are returned.
+         * @return true if the read was successful.
+         * @return false if the peer disconnected.
          */ 
-        bool read_n_bytes(int n, char* buff);
+        bool read_n_bytes(size_t n, char* buff);
 
         // Delete copy constructor and copy assignment.
         TCPHandler(TCPHandler const&) = delete;
@@ -36,8 +37,17 @@ class TCPHandler {
     private:
         int socket;
         char *recv_buff;
-        int recv_buff_size;
+        size_t recv_buff_size;
         std::deque<char> recv_deque;
+
+        /**
+         * @brief Reads from the TCP stream as long as there are not enough bytes in recv_deque.
+         * 
+         * @param n Minimum number of bytes in recv_deque when returning.
+         * @return true if the read was successful.
+         * @return false if the peer disconnected.
+         */
+        bool return_when_n_bytes_in_deque(size_t n);
 };
 
 #endif // NETWORK_HANDLER_H
